3128c6eb3287d9fcb470c3b9e25bd84d2e2ed37d
Yanzhen Yu <yanzhen@smartx.com>
2018-12-06 15:56:38 +0800

    init first demo and util components

--- src/client.js
+++ src/client.js
@@ -1,5 +1,13 @@
 import * as sapper from '../__sapper__/client.js';
+import { Store } from 'svelte/store.js';
 
 sapper.start({
-	target: document.querySelector('#sapper')
+	target: document.querySelector('#sapper'),
+	store: data => {
+		const store = new Store(data)
+		// for debug
+		window.store = store
+
+		return store
+	}
 });
--- src/server.js
+++ src/server.js
@@ -2,15 +2,24 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '../__sapper__/server.js';
+import { Store } from 'svelte/store.js';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
+
+const data = {
+	events: [],
+}
 
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
-		sapper.middleware()
+		sapper.middleware({
+			store: request => {
+				return new Store(data)
+			}
+		})
 	)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
